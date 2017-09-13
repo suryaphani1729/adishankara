7ca1b856ecce8c7b9dcf6a8c18114d9f5fdcedc3
surya <surya.phani1729@gmail.com>
2017-09-13 19:33:48 +0530

    service worker file added

---
+++ service-worker.js
@@ -0,0 +1,40 @@
+var cacheName = 'adishankaraCache-1';
+var filesToCache = [
+   '/adishankara/',
+  '/adishankara/index.html',
+  '/adishankara/main.js',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Install');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', function(e) {
+  console.log('[ServiceWorker] Fetch', e.request.url);
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
