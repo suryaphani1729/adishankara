e387df79796788b4302f652c3c9695abbd2a300e
surya <surya.phani1729@gmail.com>
2017-09-15 13:31:20 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,7 @@
-var cacheName = 'adishankaraCache-4';
+var cacheName = 'adishankaraCache-5';
+var dataCacheName = 'slokaData-v1';
+
+
 var filesToCache = [
    '/adishankara/',
   '/adishankara/index.html',
@@ -22,22 +25,30 @@ self.addEventListener('install', function(e) {
 });
 
 self.addEventListener('activate', function(e) {
-  console.log('[ServiceWorker] Activate');
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      return Promise.all(keyList.map(function(key) {
-        if (key !== cacheName) {
-          console.log('[ServiceWorker] Removing old cache', key);
-          return caches.delete(key);
-        }
-      }));
-    })
-  );
+   if (key !== cacheName && key !== dataCacheName) {
+            console.log('[ServiceWorker] Activate');
+           e.waitUntil(
+             caches.keys().then(function(keyList) {
+               return Promise.all(keyList.map(function(key) {
+                 if (key !== cacheName) {
+                   console.log('[ServiceWorker] Removing old cache', key);
+                   return caches.delete(key);
+                 }
+               }));
+             })
+           );
+   }
   return self.clients.claim();
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('[ServiceWorker] Fetch', e.request.url);
+  console.log('[ServiceWorker] Fetch', e.request.url+"/"+config.databaseURL);
+   
+   
+   
+   
+   
+   
   e.respondWith(
     caches.match(e.request).then(function(response) {
       return response || fetch(e.request);
