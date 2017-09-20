99e89086cbda8275b4531c30b17ecf69ac1a7e71
surya <surya.phani1729@gmail.com>
2017-09-20 15:24:10 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -78,3 +78,19 @@ self.addEventListener('fetch', function(e) {
     );
   }
 });
+
+
+//regarding push notifications
+
+// Register event listener for the 'push' event.
+self.addEventListener('push', function(event) {
+
+ payload = event.data.text();
+
+ // Keep the service worker alive until the notification is created.
+ event.waitUntil(
+   // Show a notification with title and use the payload
+   // as the body.
+   self.registration.showNotification(payload.title, payload.options)
+ );
+});
