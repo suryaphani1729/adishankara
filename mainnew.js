5e22f8c919271acc543e562bfa3e1e037994d56e
surya <surya.phani1729@gmail.com>
2018-06-25 20:09:31 +0530

    Update mainnew.js

--- mainnew.js
+++ mainnew.js
@@ -3,14 +3,14 @@ var app = angular.module('devotionalApp', []).controller("appCtrl",function($sco
 	$scope.data = {};
 	$scope.slokas = [{"title":"Loading...","descr" : "..."}];
 	var request = indexedDB.open("library");
-	var db;
+	$scope.db;
 	request.onsuccess = function() {
-	   db = request.result;
+	   $scope.db = request.result;
 	   $scope.getData();
 	}
 	
     $scope.getData = function(){ 
-  			var tx = db.transaction("itemlist", "readwrite");
+  			var tx = $scope.db.transaction("itemlist", "readwrite");
 			var store = tx.objectStore("itemlist");
 			var index = store.index("by_title");
 			var request = index.getAll();
