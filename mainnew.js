ac2c5a33398680feb59697b27a1161d3e95a62a8
surya <surya.phani1729@gmail.com>
2018-06-25 18:30:32 +0530

    Create mainnew.js

---
+++ mainnew.js
@@ -0,0 +1,38 @@
+var app = angular.module('devotionalApp', []).controller("appCtrl",function($scope){
+
+	$scope.data = {};
+	$scope.slokas = [{"title":"Loading...","descr" : "..."}];
+   $scope.getData = function(){ 
+	   
+   firebase.database().ref('/ShankaraProject').once('value').then(function(snapshot) {
+       $scope.data = snapshot.val();
+        
+		$scope.slokas = $scope.data.data1;
+		
+	   
+	caches.open('slokaData-v1').then(function(cache) {
+		cache.add('slokasData',$scope.slokas);
+	});
+
+
+	   
+	   
+	   
+		$scope.$apply();
+		
+		
+   });
+      
+	   
+	   
+    
+   }
+
+   
+   
+	
+	
+  $scope.getData();
+
+
+});  
